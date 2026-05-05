f18428816feeb9904d23e1d2e35c269ca2c0ec5d
Mykyta Brenhach <nikassafronovs@gmail.com>
2026-05-05 15:06:41 +0300

    add console log to handleDocumentPressKey

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,8 @@ export class App extends React.Component<{}, State> {
 
   handleDocumentPressKey = (event: KeyboardEvent) => {
     this.setState({ pressedKey: event.key });
+    // eslint-disable-next-line no-console
+    console.log(event.key);
   };
 
   componentDidMount(): void {
